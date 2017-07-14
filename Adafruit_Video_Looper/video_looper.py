e70bac48366876d6efa2861b499b0ca6303ccb35
xmr-smithx <emailforallpeoplee@gmail.com>
2017-07-14 08:22:44 -0600

    Update video_looper.py

--- Adafruit_Video_Looper/video_looper.py
+++ Adafruit_Video_Looper/video_looper.py
@@ -152,7 +152,7 @@ class VideoLooper(object):
         message if the on screen display is enabled.
         """
         # Print message to console with number of movies in playlist.
-        message = 'Found {0} movie{1}.'.format(playlist.length(), 
+        message = 'I {0} I{1}.'.format(playlist.length(), 
             's' if playlist.length() >= 2 else '')
         self._print(message)
         # Do nothing else if the OSD is turned off.
@@ -160,7 +160,7 @@ class VideoLooper(object):
             return
         # Draw message with number of movies loaded and animate countdown.
         # First render text that doesn't change and get static dimensions.
-        label1 = self._render_text(message + ' Starting playback in:')
+        label1 = self._render_text(message + ' CRY IS FREE:')
         l1w, l1h = label1.get_size()
         sw, sh = self._screen.get_size()
         for i in range(seconds, 0, -1):
