ad5f318aaf0bbd6aff4169f1aaed2e0a369a8ca3
xmr-smithx <emailforallpeoplee@gmail.com>
2017-08-03 15:34:38 -0600

    Update directory.py

--- Adafruit_Video_Looper/directory.py
+++ Adafruit_Video_Looper/directory.py
@@ -1,6 +1,8 @@
 # Copyright 2015 Adafruit Industries.
 # Author: Tony DiCola
 # License: GNU GPLv2, see LICENSE.txt
+from datetime import datetime, time
+
 class DirectoryReader(object):
 
     def __init__(self, config):
@@ -10,11 +12,21 @@ class DirectoryReader(object):
         self._load_config(config)
 
     def _load_config(self, config):
-        self._path = config.get('directory', 'path')
+        self._path1 = config.get('directory', 'path1')
+        self._path2 = config.get('directory', 'path2')
+        self._path3 = config.get('directory', 'path3')
 
-    def search_paths(self):
+    def search_paths1(self):
         """Return a list of paths to search for files."""
-        return [self._path]
+        return [self._path1]
+    
+    def search_paths2(self):
+        """Return a list of paths to search for files."""
+        return [self._path2]
+
+    def search_paths3(self):
+        """Return a list of paths to search for files."""
+        return [self._path3]
 
     def is_changed(self):
         """Return true if the file search paths have changed."""
@@ -25,9 +37,17 @@ class DirectoryReader(object):
         # not resource intensive.
         return False
 
-    def idle_message(self):
+    def idle_message1(self):
         """Return a message to display when idle and no files are found."""
-        return 'No files found in {0}'.format(self._path)
+        return 'No files found in {0}'.format(self._path1)
+    
+    def idle_message2(self):
+        """Return a message to display when idle and no files are found."""
+        return 'No files found in {0}'.format(self._path2)
+    
+    def idle_message3(self):
+        """Return a message to display when idle and no files are found."""
+        return 'No files found in {0}'.format(self._path3)
 
 
 def create_file_reader(config):
